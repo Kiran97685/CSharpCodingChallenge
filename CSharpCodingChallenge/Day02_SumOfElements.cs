2fdf6f430628518e5ac4db2520cd8732a0b01a5e
Kiran97685 <kiran.kurella97685@gmail.com>
2025-10-24 23:43:20 -0500

    sum of elements

---
+++ CSharpCodingChallenge/Day02_SumOfElements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CSharpCodingChallenge
+{
+    internal class Day02_SumOfElements
+    {
+        public void SumOfElements()
+        {
+            int[] numbers = { 10, 20, 30, 40, 50 };
+            int sum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            double average = (double)sum / numbers.Length;
+            Console.WriteLine("Average of array elements: " + average);
+
+        }
+    }
+}
