d93b6f69da2a55fdc6708f075fe522d865171873
Kiran97685 <kiran.kurella97685@gmail.com>
2025-11-02 19:57:43 -0600

    remove duplicate elements

---
+++ CSharpCodingChallenge/Day11_ArrayUnique.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CSharpCodingChallenge
+{
+    internal class Day11_ArrayUnique
+    {
+        public void PrintUnique()
+        {
+            int[] numbers = { 2, 3, 2, 4, 5, 3, 6, 4 };
+
+            Console.WriteLine("Unique elements are:");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                bool isDuplicate = false;
+
+                for (int j = 0; j < i; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+
+                if (!isDuplicate)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+        }
+    }
+}
