3dd1e23630daef2d70d33e6bd47e2a6913d9cdc6
Kiran97685 <kiran.kurella97685@gmail.com>
2025-11-12 23:37:49 -0600

    Repeated words in string

---
+++ CSharpCodingChallenge/Day21_StringRepeatedWord.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpCodingChallenge
+{
+    internal class Day23_StringMostRepeatedWord
+    {
+        public void FindMostRepeatedWord()
+        {
+            string sentence = "learning CSharp is fun and learning coding improves confidence in coding";
+
+            // Convert string → array of words
+            string[] words = sentence.Split(' ');
+
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+            // Count each word (case-insensitive)
+            foreach (string word in words)
+            {
+                string key = word.ToLower();
+
+                if (wordCount.ContainsKey(key))
+                    wordCount[key]++;
+                else
+                    wordCount[key] = 1;
+            }
+
+            // Find word with max count
+            string mostRepeated = "";
+            int maxCount = 0;
+
+            foreach (var item in wordCount)
+            {
+                if (item.Value > maxCount)
+                {
+                    mostRepeated = item.Key;
+                    maxCount = item.Value;
+                }
+            }
+
+            Console.WriteLine("Most repeated word: " + mostRepeated);
+            Console.WriteLine("Repeated count: " + maxCount);
+        }
+    }
+}
