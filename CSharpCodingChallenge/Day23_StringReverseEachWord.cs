be78fae08cdad611328bdf6c238a99583793b7fd
Kiran97685 <kiran.kurella97685@gmail.com>
2025-11-14 19:12:06 -0600

    Reverse each word in string

---
+++ CSharpCodingChallenge/Day23_StringReverseEachWord.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CSharpCodingChallenge
+{
+    internal class Day27_StringReverseEachWord
+    {
+        public void ReverseEachWord()
+        {
+            string sentence = "CSharp makes coding interesting";
+
+            // Convert string → array of words
+            string[] words = sentence.Split(' ');
+
+            Console.WriteLine("Reversed words (order same):");
+
+            foreach (string word in words)
+            {
+                string reversed = ReverseWord(word);
+                Console.Write(reversed + " ");
+            }
+        }
+
+        private string ReverseWord(string word)
+        {
+            char[] chars = word.ToCharArray();
+            Array.Reverse(chars);
+            return new string(chars);
+        }
+    }
+}
