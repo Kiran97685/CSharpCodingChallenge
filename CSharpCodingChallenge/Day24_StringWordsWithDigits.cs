1b023831d422c0246ab6355209b11cc80c7c87ad
Kiran97685 <kiran.kurella97685@gmail.com>
2025-11-15 10:09:34 -0600

    String words with digits

---
+++ CSharpCodingChallenge/Day24_StringWordsWithDigits.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CSharpCodingChallenge
+{
+    internal class Day24_StringWordsWithDigits
+    {
+        public void CountWordsWithDigits()
+        {
+            string sentence = "CSharp5 coding2025 learn2code version10 update";
+
+            // Convert string → array of words
+            string[] words = sentence.Split(' ');
+
+            int count = 0;
+
+            Console.WriteLine("Words containing digits:");
+
+            foreach (string word in words)
+            {
+                if (ContainsDigit(word))
+                {
+                    Console.WriteLine(word);
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Total words with digits: " + count);
+        }
+
+        private bool ContainsDigit(string word)
+        {
+            foreach (char ch in word)
+            {
+                if (char.IsDigit(ch))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
