3e1e4299586ceca785e9ce6c6980d7e6b1f46b40
Kiran97685 <kiran.kurella97685@gmail.com>
2025-11-16 18:58:41 -0600

    Find string with a letter

---
+++ CSharpCodingChallenge/Day25_StartsWithLetter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSharpCodingChallenge
+{
+    internal class Day25_StartsWithLetter
+    {
+        public void CountNamesStartingWithLetter()
+        {
+            string[] names = { "Ravi", "Rohan", "Meera", "Rahul", "Sneha", "maya" };
+
+            Console.Write("Enter a letter: ");
+            char letter = char.ToLower(Console.ReadLine()[0]);
+
+            int count = 0;
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (char.ToLower(names[i][0]) == letter)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Names starting with '" + letter + "': " + count);
+        }
+    }
+}
