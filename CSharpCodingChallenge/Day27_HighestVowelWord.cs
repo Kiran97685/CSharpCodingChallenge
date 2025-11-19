243db183a0440d4f17a635541e06d40dc55ca9fb
Kiran97685 <kiran.kurella97685@gmail.com>
2025-11-18 23:30:43 -0600

    Highest vowel count in a string

---
+++ CSharpCodingChallenge/Day27_HighestVowelWord.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CSharpCodingChallenge
+{
+    internal class Day27_HighestVowelWord
+    {
+        public void FindWordWithMostVowels()
+        {
+            string sentence = "education improves your understanding significantly";
+
+            string[] words = sentence.Split(' ');
+
+            string highestVowelWord = "";
+            int maxVowels = 0;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int vowelCount = 0;
+
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    char ch = char.ToLower(words[i][j]);
+
+                    if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                    {
+                        vowelCount++;
+                    }
+                }
+
+                if (vowelCount > maxVowels)
+                {
+                    maxVowels = vowelCount;
+                    highestVowelWord = words[i];
+                }
+            }
+
+            Console.WriteLine("Sentence: " + sentence);
+            Console.WriteLine("Word with highest vowels: " + highestVowelWord);
+            Console.WriteLine("Number of vowels: " + maxVowels);
+        }
+    }
+}
