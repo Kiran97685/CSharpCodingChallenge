7fa40efa942f5249e212ab93596f1bbb74b0ae72
Kiran97685 <kiran.kurella97685@gmail.com>
2025-11-24 23:15:23 -0600

    Boolean Streak Counter

---
+++ CSharpCodingChallenge/Day33_BooleanStreakCounter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CSharpCodingChallenge
+{
+    internal class Day33_BooleanStreakCounter
+    {
+        public void CountTrueStreaks()
+        {
+            bool[] flags = { true, true, false, true, false, true, true, true, false };
+
+            int streakCount = 0;
+            int currentStreak = 0;
+            int longestStreak = 0;
+
+            for (int i = 0; i < flags.Length; i++)
+            {
+                if (flags[i] == true)
+                {
+                    currentStreak++;
+
+                    if (currentStreak == 1)
+                    {
+                        streakCount++; // New streak found
+                    }
+
+                    if (currentStreak > longestStreak)
+                    {
+                        longestStreak = currentStreak;
+                    }
+                }
+                else
+                {
+                    currentStreak = 0; // Reset streak
+                }
+            }
+
+            Console.WriteLine("Total TRUE streaks: " + streakCount);
+            Console.WriteLine("Longest TRUE streak: " + longestStreak);
+        }
+    }
+}
