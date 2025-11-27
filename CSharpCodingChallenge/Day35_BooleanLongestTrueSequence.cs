b62737c1e840a3cf44174595ecacaab7269e8890
Kiran97685 <kiran.kurella97685@gmail.com>
2025-11-26 23:19:52 -0600

    Boolean Longest True Sequence

---
+++ CSharpCodingChallenge/Day35_BooleanLongestTrueSequence.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharpCodingChallenge
+{
+    internal class Day35_BooleanLongestTrueSequence
+    {
+        public void FindLongestTrueSequence()
+        {
+            bool[] flags = { true, true, false, true, true, true, false, true };
+
+            int currentCount = 0;
+            int longestCount = 0;
+
+            for (int i = 0; i < flags.Length; i++)
+            {
+                if (flags[i] == true)
+                {
+                    currentCount++;
+
+                    if (currentCount > longestCount)
+                        longestCount = currentCount;
+                }
+                else
+                {
+                    currentCount = 0; // reset sequence
+                }
+            }
+
+            Console.WriteLine("Longest consecutive TRUE sequence: " + longestCount);
+        }
+    }
+}
