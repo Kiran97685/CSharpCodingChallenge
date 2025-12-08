c1d16de908527b62a60f1bf4b0fc3e125fe66071
Kiran97685 <kiran.kurella97685@gmail.com>
2025-12-08 16:11:19 -0600

    Struct car speed array

---
+++ CSharpCodingChallenge/Day47_StructCarSpeedArray.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CSharpCodingChallenge
+{
+    internal class Day47_StructCarSpeed
+    {
+        // Define a struct (custom data type)
+        struct Car
+        {
+            public string Brand;
+            public int HorsePower;
+            public int TopSpeed;
+
+            public Car(string brand, int hp, int speed)
+            {
+                Brand = brand;
+                HorsePower = hp;
+                TopSpeed = speed;
+            }
+        }
+
+        public void FindFastestCar()
+        {
+            // Array of struct (NOT int, NOT string, NOT tuple)
+            Car[] cars = new Car[]
+            {
+                new Car("BMW", 450, 280),
+                new Car("Audi", 420, 260),
+                new Car("Mercedes", 500, 300),
+                new Car("Ferrari", 700, 350),
+                new Car("Lamborghini", 750, 355)
+            };
+
+            Car fastest = cars[0];
+
+            foreach (Car c in cars)
+            {
+                if (c.TopSpeed > fastest.TopSpeed)
+                {
+                    fastest = c;
+                }
+            }
+
+            Console.WriteLine("Fastest Car Details:");
+            Console.WriteLine("Brand: " + fastest.Brand);
+            Console.WriteLine("HorsePower: " + fastest.HorsePower);
+            Console.WriteLine("Top Speed: " + fastest.TopSpeed + " km/h");
+        }
+    }
+}
