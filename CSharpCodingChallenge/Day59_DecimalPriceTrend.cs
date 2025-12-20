d2feb9114c6a34c81cff34a41f16c9eb7e5d14d0
Kiran97685 <kiran.kurella97685@gmail.com>
2025-12-20 09:54:41 -0600

    Decimal Price Trend

---
+++ CSharpCodingChallenge/Day59_DecimalPriceTrend.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CSharpCodingChallenge
+{
+    internal class Day59_DecimalPriceTrend
+    {
+        public void AnalyzePriceTrend()
+        {
+            decimal[] prices = { 100.50m, 102.75m, 101.25m, 101.25m, 105.00m };
+
+            Console.WriteLine("Price Trend Analysis:");
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] > prices[i - 1])
+                {
+                    Console.WriteLine($"Day {i}: Price Increased");
+                }
+                else if (prices[i] < prices[i - 1])
+                {
+                    Console.WriteLine($"Day {i}: Price Decreased");
+                }
+                else
+                {
+                    Console.WriteLine($"Day {i}: Price Unchanged");
+                }
+            }
+        }
+    }
+}
