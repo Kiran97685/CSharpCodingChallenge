f42f6f2a93b938924bde57f6e948c32a80ef7a64
Kiran97685 <kiran.kurella97685@gmail.com>
2025-12-23 22:50:44 -0600

    Guid Array Duplicate Check

---
+++ CSharpCodingChallenge/Day62_GuidArrayDuplicateCheck.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CSharpCodingChallenge
+{
+    internal class Day62_GuidArrayDuplicateCheck
+    {
+        public void FindDuplicateGuids()
+        {
+            Guid[] ids =
+            {
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                Guid.NewGuid()
+            };
+
+            // Intentionally creating duplicates
+            ids[2] = ids[0];
+            ids[3] = ids[1];
+
+            Console.WriteLine("Duplicate GUIDs:");
+
+            for (int i = 0; i < ids.Length; i++)
+            {
+                int count = 1;
+
+                if (ids[i] == Guid.Empty)
+                    continue;
+
+                for (int j = i + 1; j < ids.Length; j++)
+                {
+                    if (ids[i] == ids[j])
+                    {
+                        count++;
+                        ids[j] = Guid.Empty; // mark as visited
+                    }
+                }
+
+                if (count > 1)
+                {
+                    Console.WriteLine(ids[i] + " → Count: " + count);
+                }
+            }
+        }
+    }
+}
