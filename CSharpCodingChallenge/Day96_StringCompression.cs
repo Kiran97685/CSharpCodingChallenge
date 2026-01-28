ceed91244a7a5cd48e86adf42e2f25c76a888aa0
Kiran97685 <kiran.kurella97685@gmail.com>
2026-01-27 23:51:12 -0600

    String Compression

---
+++ CSharpCodingChallenge/Day96_StringCompression.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CSharpCodingChallenge
+{
+    internal class Day96_StringCompression
+    {
+        public void CompressString()
+        {
+            string input = "aaabbccccdaa";
+
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("Empty string");
+                return;
+            }
+
+            string result = "";
+            int count = 1;
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i] == input[i - 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    result += input[i - 1] + count.ToString();
+                    count = 1;
+                }
+            }
+
+            // Add last character
+            result += input[input.Length - 1] + count.ToString();
+
+            Console.WriteLine("Compressed string: " + result);
+        }
+    }
+}
